0ca19da620f5f52875965c215460b993bfa50806
sarikachaudhari <sarikach244@gmail.com>
2017-09-27 12:21:45 +0530

    created owner reg & login html pages

--- static/js/app.js
+++ static/js/app.js
@@ -6,15 +6,25 @@ angular.module('transportApp', ['transportApp.controllers','ui.router'])
 		templateUrl:'../html_templates/login.html',
 // controller:'firstCtrl'
 })
-	.state('TruckDetails', {
-		url: '/TruckDetails',
-		templateUrl:'../html_templates/Truck_Details.html',
-		controller:'Truck_detailsCtrl'
+// 	.state('TruckDetails', {
+// 		url: '/TruckDetails',
+// 		templateUrl:'../html_templates/Truck_Details.html',
+// 		controller:'Truck_detailsCtrl'
+// })
+
+	.state('Owner_Reg', {
+		url: '/Owner_Reg',
+		templateUrl:'../html_templates/Owner_Reg.html',
+		controller:'transportCtrl'
 })
+
 	.state('Truckowner', {
 		url: '/Truckowner',
-		templateUrl:'../html_templates/Truckowner.html',
-// controller:'firstCtrl'
+		templateUrl:'../html_templates/Owner_Login.html',
+		// controller:'transportCtrl'
 });
+
+
+
 	$urlRouterProvider.otherwise('/');
 }); //config ends
--- static/js/transportController.js
+++ static/js/transportController.js
@@ -1,4 +1,19 @@
 angular.module('transportApp.controllers',[])
 .controller('transportCtrl', function($log,$scope){
 console.log("DesignerCategoryGallery loaded...");
-});
+
+$scope.owner_submit= { owner:null,
+            email:null, 
+            contact:null,
+            address:null,
+            maxtrcuk:null,
+            city:null,
+            trucktype:null,
+            };
+
+        $scope.saveData  = function() {
+        console.log('user',$scope.owner_submit);
+}
+}
+
+);
